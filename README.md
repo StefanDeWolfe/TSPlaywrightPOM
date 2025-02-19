f07ec2e84711be92211f26ee53bb819c580950f1
Stefan DeWolfe <stefandewolfe@gmail.com>
2025-02-19 11:15:27 -0800

    Added Product Checkout e2e test

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ This is an example of a Playwright test framework project
 https://www.saucedemo.com/
 
 # Run test commands
-Some comamnds to run to see if its working
+Some commands to run to see if its working
 ```
 npx playwright test [test file]
 npx playwright test login.spec.ts --project firefox --headed
 npx playwright test -g "Login as a standard user"
 npx playwright test -g "Login as a standard user" --project firefox --headed
 ```
-How I set this up (for future me).
+
+# How I set this up (for future me)
 ```
 npm init playwright@latest
 ```
@@ -29,3 +30,4 @@ Visited "https://playwright.dev/docs/pom" for POM page example
 
 # Login Test: login.spec.js
 Basic login tests.
+-   Login as a standard user
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 import { ProductsPage } from '../pages/products-page';
 
 test.beforeEach(async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  await loginPage.goto();
+  const login = new LoginPage(page)
+  await login.goto();
 });
 
 test('Login as a standard user', async ({ page }) => {
@@ -13,7 +13,15 @@ test('Login as a standard user', async ({ page }) => {
   const username = process.env.USER_NAME ?? "standard_user"
   const password = process.env.PASSWORD ?? "secret_sauce"
   await loginPage.login(username, password)
+  await productsPage.verifyPageTitle();
+});
+
+test('Login as a locked out user', async ({ page }) => {
+  const login = new LoginPage(page)
+  const products = new ProductsPage(page)
+  const username = "locked_out_user"
+  const password = process.env.PASSWORD ?? "secret_sauce"
+  await login.login(username, password)
   await page.waitForLoadState(); // The promise resolves after 'load' event.
-  await page.waitForURL('**/inventory.html');
-  await productsPage.verifyHeader();
+  await login.expectLockout()
 });
